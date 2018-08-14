78d1f832962d234a3add7ce4ef96cfb77635171d
sagittaria <shuyang_x@hotmail.com>
2018-08-14 23:24:04 +0800

    resolve vulnerability

--- src/components/master/master.vue
+++ src/components/master/master.vue
@@ -6,8 +6,8 @@
         <el-menu :router="true"
           :default-active="activeMenuItem"
           :active="activeMenuItem">
-          <el-menu-item index="/master/post/all">全部</el-menu-item>
-          <el-menu-item index="/master/post/add">新增</el-menu-item>
+          <el-menu-item index="/master/post/all"><i class="el-icon-view"></i>全部</el-menu-item>
+          <el-menu-item index="/master/post/add"><i class="el-icon-edit"></i>新增</el-menu-item>
         </el-menu>
       </el-aside>
       <el-main><router-view/></el-main>
