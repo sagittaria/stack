beb929012fee67224522bca8cc6687d7087a4074
盐生 <shuyang_x@hotmail.com>
2018-08-15 10:44:10 +0800

    404page-测试push

--- src/components/Error404.vue
+++ src/components/Error404.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>404</div>
+  <div>
+    <h1>404 <small><a href="/" title="GET LOST">╮(╯▽╰)╭</a></small></h1>
+  </div>
 </template>
+
+<style scoped>
+  h1{font-size:100px;line-height:1em;margin:180px;font-weight:lighter;text-align:center;}
+  a{font-size:1.4rem;text-decoration: none; color: #878787;}
+</style>
